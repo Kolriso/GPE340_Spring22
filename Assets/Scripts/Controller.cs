3673c56807ac33398721a612d8f05ab3fc5f89da
Kolriso Biddle <kolbiddl@uat.edu>
2022-01-24 23:32:34 -0700

    Created pawn & controller scripts, materials & added them to scene

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Pawn pawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Send my move command to my pawn
+        pawn.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+    }
+}
