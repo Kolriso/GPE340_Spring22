d88c5221b23e0d58aaad9dd997bcf51b33973f85
Kolriso Biddle <kolbiddl@uat.edu>
2022-01-27 11:15:19 -0700

    Created a camera controller to follow the character

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Pawn player; // A variable to store a reference to the player game object
+
+    private Vector3 offset; // A variable to store the offset distance between the player and camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hook up the GameObject to the pawn
+        if (player == null) Debug.LogWarning ("Character not connected"); 
+
+       // Calculate and store the offset value by getting the distance between the player's position and camera's position.
+       offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
