8ea8a1d8ede648b43e0dd1fe6401ed18a17aed93
Kolriso Biddle <kolbiddl@uat.edu>
2022-01-27 14:41:50 -0700

    Added the rotate function for both the pawn script and the controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public Pawn pawn;
+    public Pawn pawn; // Created a Pawn variable to get access to the pawn
+    public Camera playerCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (playerCamera == null) Debug.LogWarning("Error: No camera set!");
     }
 
     // Update is called once per frame
     void Update()
     {
         // Send my move command to my pawn
-        pawn.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+        Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // Limit the vector magnitude to 1
+        moveVector = Vector3.ClampMagnitude(moveVector, 1.0f);
+        // Tell the pawn to move
+        pawn.Move(moveVector);
+
+        // Rotate player to face the mouse
+        RotateToMouse();
+    }
+
+    void RotateToMouse()
+    {
+        // Create a plane object (a mathematical representation of all the point in 2D)
+        Plane groundPlane;
+
+        // Set that plane so it is the X,Z plane the player is standing on
+        groundPlane = new Plane(Vector3.up, pawn.transform.position);
+
+        // Cast a ray from our camera toward the plane, through our mouse cursor
+        float distance; // Variable to hold the distance from the ray to the ground plane
+        Ray cameraRay = playerCamera.ScreenPointToRay(Input.mousePosition); // Creating a camera ray from the mouse position
+        groundPlane.Raycast(cameraRay, out distance); // Figuring out the distance from the camera ray to the gorund plane
+
+        // Find where that ray hits the plane
+        Vector3 raycastPoint = cameraRay.GetPoint(distance);
+
+        // Rotate towards that point 
+        pawn.RotateTowards(raycastPoint);
     }
 }
--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
-    private Animator anim;
+    private Animator anim; // Creating an animator variable
+    public float moveSpeed = 1; // Meters per second
+    public float rotateSpeed = 360; // Degrees per second
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,25 @@ public class Pawn : MonoBehaviour
 
     public void Move(Vector3 moveVector)
     {
+        // Apply speed
+        moveVector = moveVector * moveSpeed;
+
+        // Set the animation to move on the x axis
         anim.SetFloat("Right", moveVector.x);
+        // Set the animation to move on the z axis
         anim.SetFloat("Forward", moveVector.z);
+    }
+
+    public void RotateTowards(Vector3 targetPoint)
+    {
+        // Find the roation that would be looking at that target point
+        // Find the vector to the point
+        Vector3 targetVector = targetPoint - transform.position;
+
+        // Find roatation down that vector
+        Quaternion targetRotation = Quaternion.LookRotation(targetVector, Vector3.up);
+
+        // Change my roation (slowly) towards that targeted roation
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
     }
 }
