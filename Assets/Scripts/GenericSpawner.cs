45ca85a68e563cb2711cf4e73ff2bfa3822ca87b
Kolriso Biddle <kolbiddl@uat.edu>
2022-03-20 17:13:50 -0700

    Created enemy spawners and put them in the scene

--- Assets/Scripts/GenericSpawner.cs
+++ Assets/Scripts/GenericSpawner.cs
@@ -6,6 +6,7 @@ public class GenericSpawner : MonoBehaviour
 {
     [Header("Game Objects")]
     public GameObject objectToSpawn;
+    public Mesh spawnMesh;
     [Header("Data")]
     public float respawnTime;
 
@@ -36,10 +37,20 @@ public class GenericSpawner : MonoBehaviour
             if (countDown <= 0)
             {
                 // Spawn (and store) the object
-                spawnedObject = Instantiate(objectToSpawn, transform.position, transform.rotation);
+                spawnedObject = Instantiate(objectToSpawn, transform.position, transform.rotation) as GameObject;
                 // Reset the countdown
                 countDown = respawnTime;
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Matrix4x4 myNewCoordinateSystem = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Gizmos.color = Color.Lerp(Color.red, Color.clear, 0.7f);
+        Gizmos.matrix = myNewCoordinateSystem;
+        Gizmos.DrawMesh(spawnMesh, new Vector3(0, 0, 0), Quaternion.identity);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(Vector3.zero, Vector3.forward);
     }
 }
