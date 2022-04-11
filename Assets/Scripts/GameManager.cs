61f261a4b99e2c988eb8e406286af2eecfed2d35
Kolriso Biddle <kolbiddl@uat.edu>
2022-04-10 20:43:50 -0700

    I created a game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class represents the game! So: Everything that is in our game should be accessable from here.
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        // The first Game manager
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // There's already a game manager, so destroy myself
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Show main menu
+    }
+
+    // Start game starts when gameplay starts
+    public void StartGame()
+    {
+        
+    }
+
+    public void EndGame()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
